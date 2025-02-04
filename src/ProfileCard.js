25911f7578eef112815ac523b4ab7e0f2cd6f382
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-02-04 21:53:09 +1100

    Destructuring - props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,22 @@
 function ProfileCard(props){
+
+    //--const title = props.title ;
+    //--const handle = props.handle ;
+
+    const {title, handle} = props;
+
     return (
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
             <br />
         </div>        
     );
 }
 
 export default ProfileCard;
+
+// -- Make code shorter and cleaner.
+// -- Easier to read and understand.
+// -- Prevents unnecessary repitition.
+// -- function ProfileCard({title, handle}){}
