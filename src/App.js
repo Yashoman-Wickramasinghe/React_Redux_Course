a437b098b6b1c98f5b336cd4a394ed994f980308
Yasho Wickramasinghe <yashoman0608@gmail.com>
2025-02-05 11:38:43 +1100

    React_image_handling and add css Bulma

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App(){
 
   return (
       <div>
-        <div>Personal Digital Assistant.</div>
+        <section class="hero is-primary">
+          <div class="hero-body">
+            <p class="title">Personal Digital Assistant.</p>
+          </div>
+        </section>
 
-        <ProfileCard title ="Alexa" handle="@alexa99" />
-        <ProfileCard title ="Cortana" handle="@cortana32" />
-        <ProfileCard title ="Alexa" handle="@siri01" />
-        
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+                <div className="column is-4">
+                  <ProfileCard title ="Alexa" handle="@alexa99" image={AlexaImage} description="Alexa was created by Amazon and helps you buy things." />
+                </div>
+                <div className="column is-4">
+                  <ProfileCard title ="Cortana" handle="@cortana32" image={CortanaImage} description="Cortana was created by Microsoft.Who knows what it does?" />
+                </div>
+                <div className="column is-4">
+                  <ProfileCard title ="Alexa" handle="@siri01" image={SiriImage} description="Siri was created by Apple and is being phased out." />
+                </div>
+            </div>
+          </section>
+        </div>
       </div>
   );
 
